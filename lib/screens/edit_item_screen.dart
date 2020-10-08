56a372415d41f0656c8760e92a216af4adbca407
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-10-08 11:27:56 +0500

    Date Picker added.

--- lib/screens/edit_item_screen.dart
+++ lib/screens/edit_item_screen.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app/widgets/date_picker.dart';
 
-class EditItemScreen extends StatelessWidget {
+class EditItemScreen extends StatefulWidget {
   static const routeName = '/edit-item-screen';
+
+  @override
+  _EditItemScreenState createState() => _EditItemScreenState();
+}
+
+class _EditItemScreenState extends State<EditItemScreen> {
+  final FocusNode titleFocusNode = FocusNode();
+  final FocusNode descriptionFocusNode = FocusNode();
+
+  final defaultPadding = Get.width * 0.03;
+
+  DateTime _dateResponse = DateTime.now();
+
+  void setDateResponseCallBack(DateTime data) {
+    _dateResponse = data;
+    print('call back: $_dateResponse');
+  }
+
+  @override
+  void initState() {
+    titleFocusNode.requestFocus();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final defaultPadding = Get.width * 0.03;
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Item'),
@@ -22,8 +47,21 @@ class EditItemScreen extends StatelessWidget {
           child: ListView(
             children: [
               TextFormField(
+                focusNode: titleFocusNode,
                 keyboardType: TextInputType.name,
                 decoration: const InputDecoration(labelText: 'Title'),
+                onFieldSubmitted: (value) =>
+                    descriptionFocusNode.requestFocus(),
+                textInputAction: TextInputAction.next,
+              ),
+              DatePicker(
+                setDateResponse: setDateResponseCallBack,
+              ),
+              TextFormField(
+                focusNode: descriptionFocusNode,
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(labelText: 'Description'),
               ),
             ],
           ),
---
+++ lib/widgets/date_picker.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DatePicker extends StatefulWidget {
+  final void Function (DateTime dateResponseData) setDateResponse;
+  DatePicker({
+    @required this.setDateResponse,
+  });
+
+  @override
+  _DatePickerState createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  DateTime _dateResponse = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+        border: Border.all(
+          color: Theme.of(context).primaryColor,
+          width: 3,
+        ),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 30),
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '${DateFormat('MMMM dd, yyyy').format(_dateResponse)}',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+          ),
+          IconButton(
+            icon: const Icon(Icons.calendar_today),
+            onPressed: () async {
+              final errorCaseDate = _dateResponse;
+              _dateResponse = await showDatePicker(
+                context: context,
+                initialDate: _dateResponse,
+                firstDate: _dateResponse.isAfter(DateTime.now())
+                    ? DateTime.now()
+                    : _dateResponse,
+                lastDate: DateTime(2100),
+              );
+              //print(dateResponse);
+              if (_dateResponse != null) {
+                setState(() {
+                  widget.setDateResponse(_dateResponse);
+                });
+              } else {
+                setState(
+                  () {
+                    _dateResponse = errorCaseDate;
+                     widget.setDateResponse(_dateResponse);
+                  },
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
